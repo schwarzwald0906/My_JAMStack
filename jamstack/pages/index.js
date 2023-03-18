144d8b39d864b02d6f662e43e8eb538f283778ae
schwarzwald0906 <schwarzwald0906@gmail.com>
2023-03-18 16:15:08 +0900

    レイアウト整形

--- jamstack/pages/index.js
+++ jamstack/pages/index.js
@@ -1,13 +1,16 @@
 import React from 'react'
 import Hero from 'components/hero'
+import Container from 'components/container'
 
 export default function Home() {
   return (
-    <Hero
-      title="整形外科や整骨院に行っても完治しなかった方
+    <Container>
+      <Hero
+        title="整形外科や整骨院に行っても完治しなかった方
   身体のパフォーマンスを向上させたい方"
-      subtitle="あなたの思いを叶える施術院が、ここ桜木町にあります"
-      imageOn
-    />
+        subtitle="あなたの思いを叶える施術院が、ここ桜木町にあります"
+        imageOn
+      />
+    </Container>
   )
 }
