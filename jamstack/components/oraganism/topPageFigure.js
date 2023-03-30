689beef54dbc4d90b1fc10b833250e6fe12afb3d
schwarzwald0906 <schwarzwald0906@gmail.com>
2023-03-31 08:45:18 +0900

    トップページの図の作成完了

--- jamstack/components/oraganism/topPageFigure.js
+++ jamstack/components/oraganism/topPageFigure.js
@@ -4,17 +4,23 @@ export default function TopPageFigure() {
   return (
     <>
       <div className={style.container}>
-        <div className={style.circle}>
+        <div className={style.brownCircle}>
           <span> {['神経系や', <br />, 'インナーの不調']}</span>
         </div>
         <div className={style.triangle}></div>
-        <div className={style.circle}>
+        <div className={style.pinkBrownCircle}>
           <span>身体の歪み</span>
         </div>
         <div className={style.triangle}></div>
-        <div className={style.circle}>
+        <div className={style.pinkRedCircle}>
           <span>痛み・しびれ</span>
         </div>
+      </div>
+      <div className={style.whiteRectangle}>
+        <span>他院の施術</span>
+      </div>
+      <div className={style.yellowRectangle}>
+        <span>FLUXUS LOCUSの施術</span>
       </div>
     </>
   )
